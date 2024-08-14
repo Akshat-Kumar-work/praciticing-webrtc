a4738206d070b2b3ebbdb9743f56c9aa837481c2
Akshat <iamakku0.0.0.1@gmail.com>
2024-08-14 12:40:29 +0530

    added create offer

--- frontend/src/Sender.tsx
+++ frontend/src/Sender.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react"
 
 
 const Sender = () => {
+
+  const [socket,setSocket] = useState<WebSocket | null>(null);
+
+  useEffect( ()=>{
+    const socket = new WebSocket('ws://localhost:8080');
+    setSocket(socket);
+    socket.onopen = ()=>{
+      socket.send(JSON.stringify({type:'sender'}));
+    }
+  },[]);
+
+  const handleSendVideo = async()=>{
+
+    //create web rtc peer connection
+    const connection = new RTCPeerConnection();
+    const offer = await connection.createOffer();
+    await connection.setLocalDescription(offer);
+    socket?.send(JSON.stringify({type:'create-offer',sdp:connection.localDescription}));
+
+  }
+
   return (
     <div>
       Sender
-      
+
+
+      <button onClick={handleSendVideo}>Send Video</button>
+
     </div>
   )
 }
